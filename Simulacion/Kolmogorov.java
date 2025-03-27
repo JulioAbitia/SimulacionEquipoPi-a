7871c907f51b2c03d3c5b0426db27cbb286c0046
Hanna Amador <152832888+Hannaaa47@users.noreply.github.com>
2025-03-27 01:59:41 +0000

    Poniendo bonito a kolmogorov

--- Simulacion/Kolmogorov.java
+++ Simulacion/Kolmogorov.java
@@ -6,30 +6,34 @@ import java.util.Scanner;
 public class Kolmogorov {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        //creo mis variables
         ManipularCSV archivo = new ManipularCSV(); // Crear objeto
         archivo.leerArchivo("/workspaces/SimulacionEquipoPi-a/Simulacion/Datos.csv"); 
-        int n = archivo.getContador();
-        double error;
-
-        System.out.println("Ingrese el error: ");
-        error = input.nextDouble();
-        input.close();
         ArrayList<Double> serie = archivo.ListaValores(); // Usar ArrayList directamente
-        Collections.sort(serie); // Ordenar el ArrayList
-  
+        int n = archivo.getContador();
+        Scanner input = new Scanner(System.in);
+        double error;
         double[] fi = new double[n];
         double[] Ui = new double[n];
+        
+        //leo el error
+        System.out.print("Ingrese el error: ");
+        error = input.nextDouble();
+        input.close();
 
-        recursivo(serie, fi, Ui, 0, n, 1.0 / n, (error / 100.00), Double.NEGATIVE_INFINITY); // Inicializar d1 como el valor mínimo posible
+        // Ordenar el ArrayList
+        Collections.sort(serie); 
+
+        //llamo al metodo kolmogorv
+        kolmogorv(serie, fi, Ui, 0, n, 1.0 / n, (error / 100.00), Double.NEGATIVE_INFINITY); 
     }
 
-    static void recursivo( ArrayList<Double> serie, double[] fi, double[] Ui, int index, int n, double fn, double a, double d1) {
+    static void kolmogorv( ArrayList<Double> serie, double[] fi, double[] Ui, int index, int n, double fn, double a, double d1) {
         if (index < n) {
             fi[index] = (index + 1) * fn;
             Ui[index] = serie.get( index) - fi[index];
             d1 = Math.max(d1, Ui[index]); // Actualizar d1 si Ui[index] es mayor
-            recursivo(serie, fi, Ui, index + 1, n, fn, a, d1);
+            kolmogorv(serie, fi, Ui, index + 1, n, fn, a, d1);
         } else {
             double d2 = calcularH1(n, a);
             imprimirResultados(serie, fi, Ui, n, a, fn, d1, d2);
@@ -48,17 +52,17 @@ public class Kolmogorov {
     static void imprimirResultados(ArrayList<Double> serie, double[] fi, double[] Ui, int n, double a, double fn, double d1, double d2) {
         System.out.println("n=" + n);
         System.out.println("Error= " + (a * 100));
-        System.out.println("Fn= " + fn);
-        System.out.println("D1= " + d1);
-        System.out.println("D2= " + d2);
-        System.out.println("------------------------------------------------");
-        System.out.println("| i     | Valor       | Fi(Ui)      | Ui - Fi(Ui)|");
-        System.out.println("------------------------------------------------");
+        System.out.printf("Fn= %.5f%n", fn);
+        System.out.printf("D1= %.5f%n", d1);
+        System.out.printf("D2= %.5f%n", d2);
+        System.out.println("--------------------------------------------------");
+        System.out.println("| i     | Valor      | Fi(Ui)     | Ui - Fi(Ui)  |");
+        System.out.println("--------------------------------------------------");
         for (int i = 0; i < serie.size(); i++) {
-            System.out.printf("| %-5d | %-10.5f | %-10.5f | %-10.5f |%n", (i + 1), serie.get(i), fi[i], Ui[i]);
+            System.out.printf("| %-5d | %-10.5f | %-10.5f | %-12.5f |%n", (i + 1), serie.get(i), fi[i], Ui[i]);
         }
-        System.out.println("------------------------------------------------");
-
+        System.out.println("--------------------------------------------------");
+        System.out.println("\nConclusión");
         if (d1 < d2) {
             System.out.println("No hay evidencia suficiente para decir que estos numeros no estan distribuidos uniformemente");
         } else {
